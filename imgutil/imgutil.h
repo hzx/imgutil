a31dcedc12d3bbad2dd7a82916670a61d6c968bb
Dmitry Dementev <dem@oldschooldev.com>
2015-05-06 03:09:43 +0300

    initial commit

---
+++ imgutil/imgutil.h
@@ -0,0 +1,30 @@
+#ifndef IMGUTIL_H
+#define IMGUTIL_H
+
+
+#include <string>
+#include <vector>
+
+
+using std::string;
+using std::vector;
+
+
+const int IMAGE_ERROR_OK = 0;
+const int IMAGE_ERROR_READ = 1;
+const int IMAGE_ERROR_SIZES = 2;
+const int IMAGE_ERROR_SMALLER = 3;
+
+
+// resize src image to width, height size
+// rationWidth - save ratio by width, real height not equal to height
+// or save ratio by height, real width not equal to width
+// return error code
+int resizeImage(string dest, string src, size_t width, size_t height);
+/* int resizeGif(string dest, string src, size_t width, size_t height); */
+/* int resizeSmart(string dest, string src, size_t width, size_t height); */
+/* int resizeSmartBunch(string dest, string src, vector<size_t> sizes); */
+int resizeThumb(string dest, string src, size_t width, size_t height);
+
+
+#endif /* end of include guard: IMGUTIL_H */
